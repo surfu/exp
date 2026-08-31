23461a14e54d9f934798adb2b7a795392cf35d80
surfu <krevsunaliona@gmail.com>
2026-08-31 16:34:12 +0300

    wotking

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 while cap.isOpened():
